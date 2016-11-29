2a1423222573b5e6db55da2f3c02ccbeb3405085
sfnewzgirl <YOUR_GITHUB_EMAIL_ADDRESS>
2016-11-29 14:32:16 -0800

    mocks up users controller on search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,28 @@
 class UsersController < ApplicationController
   def index
+
   end
 
   def edit
+
   end
 
   def update
+
   end
+
+  # mocks up search for user by email and token
+  # def user_search(email, token)
+  #   user = Token.consume(token)
+  #   if user = nil
+  #     flash[:error] = "Your account can not be validated."
+  #   elsif user.email == params[:email]
+  #     toke = Token.generate(user)
+  #     render edit_user_path
+  #   else
+  #     flash[:error] = "Your account can not be validated."
+  #     redirect root_path
+      # end
+  # end
 
 end

6f152589654e61aff1c57346528f16b5bd7f99f5
sfnewzgirl <YOUR_GITHUB_EMAIL_ADDRESS>
2016-11-29 14:02:55 -0800

    adds third user and two Tokens

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,6 @@
 
 User.create({ email: 'johndoe@example.com', name: 'John Doe', receiveMarketing: true, receiveArticles: false, receiveDigest: true })
 User.create({ email: 'janedoe@example.com', name: 'Jane Doe', receiveMarketing: false, receiveArticles: true, receiveDigest: false })
+User.create({ email: 'misha@example.com', name: 'Misha LeClair', receiveMarketing: false, receiveArticles: true, receiveDigest: true })
+Token.create({ nonce: '775bc785-d7d0-441f-b324-7d48157ec980', user_id: 2})
+Token.create({ nonce: 'e152cd41-60ad-413a-8ac4-b38fb1977cab', user_id: 1})

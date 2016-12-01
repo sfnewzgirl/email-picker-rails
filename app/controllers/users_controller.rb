4de21e20c6f506f61b59ac00d9e92f30954e0eb5
sfnewzgirl <YOUR_GITHUB_EMAIL_ADDRESS>
2016-12-01 09:20:21 -0800

    adds more methods to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,46 @@
 class UsersController < ApplicationController
 
   def index
+    @users = User.all
   end
 
-  def search
-    @user_input = params[:q]
-    @found_user = User.all.select{|user| user.email == @user_input}
-    redirect_to edit_user_path(@found_user)
+  def new
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:alert] = 'Your profile has been saved.'
+      redirect_to edit_user_path
+    else
+      flash[:error] = 'Something went wrong. Please try again.'
+      redirect_to root_path
+    end
+  end
+
+  def show
+    @user = User.find_by_id(params[:id])
 
   def edit
-    @user = User.find_by(params[:id])
+    @user = User.find_by_id(params[:id])
   end
 
+  def update
+    @user = User.find_by(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = 'Your email preferences have been saved.'
+      redirect_to root_path
+    else
+      flash[:notice] = 'Something went wrong. Please try again.'
+      redirect_to edit_user_path(@user)
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :receiveMarketing, :receiveArticles, :receiveDigest)
+    end
+  end
 end
--- app/models/token.rb
+++ app/models/token.rb
@@ -1,9 +1,13 @@
 class Token < ApplicationRecord
   belongs_to :user
+  class << self
+    def self.generate(user)
+      token = Token.new(:user_id => user, :nonce => SecureRandom.uuid)
+      token.save
+    end
 
-  def self.generate(user)
-    token = Token.new(:user_id => user, :nonce => SecureRandom.uuid)
-    token.save
+    def consume(token)
+    # _consume_, which looks for a Token by its nonce. If it finds the token, it should delete it and return the associated User object. Otherwise it should return nil.
+    end
   end
-  
 end

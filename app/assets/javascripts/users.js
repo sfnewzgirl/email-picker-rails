6459247a45b9f3549d42f7afd5aa06b7dbd5812d
sfnewzgirl <YOUR_GITHUB_EMAIL_ADDRESS>
2016-12-02 17:52:56 -0800

    fixes opt out button click

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,13 +2,11 @@ $(document).on('turbolinks:load',
   function() {
     console.log('Sanity check: turbolinks has loaded users.js.');
 
-  function emailOptOut() {
-    jQuery('#opt_out_true').on('click', function() {
-      console.log('opt out button clicked');
-      if ($('#opt_out_true').val()) {
-        $('#check_box').val(false)
-      }
-    })
-  }
+  jQuery('#opt_out_true').on('click', function (event) {
+    console.log('opt out button clicked');
+    if ($('#opt_out_true').val()) {
+      $('#check_box').val(false)
+    }
+  })
 
 })
